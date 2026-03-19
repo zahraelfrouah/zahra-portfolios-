c83bb8106a9250366a05e52a035ea083e65f253c
elfrouazahra <elfrouahzahra@gmail.com>
2026-03-19 19:53:09 +0000

    Add Projects section and ActiveSection context; enhance About and Header components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ZAHRA PORTFOLIO",
+  title: "ZAHRA's PORTFOLIO",
   description:
     "Zahra Portfolio is a personal website showcasing the work and projects of Zahra.",
 };
@@ -16,15 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-1250 pt-28 sm:pt-36` }>
 
         <div className="bg-[#fbe2e3] absolute -top-24 right-44 h-125 w-125 rounded-full blur-[10rem] sm:w-275 -z-10"></div>
         <div className="bg-[#dbd7fb] absolute -top-4 -left-140 h-125 w-200 rounded-full blur-[10rem] sm:w-275  md:-left-132 lg:-left-152  xl:-left-60 2xl:-left-20 -z-10">
 
         </div>
+        <ActiveSectionContextProvider>
+          
         <Header />
         {children}
+        </ActiveSectionContextProvider>              
         
         </body>
     </html>
