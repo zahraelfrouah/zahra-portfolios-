1943445f9952585dbe9a9e10d2f0728b404937b1
elfrouazahra <elfrouahzahra@gmail.com>
2026-03-22 22:58:04 +0100

    feat: add Footer component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         {children}
 
         <Toaster position="top-right"/>
+        <Footer />
         </ActiveSectionContextProvider>              
         
         </body>
